35d148abaae66eda47896a6f2b98e6fbd823369c
Nazim Boudeffa <boudeffa@gmail.com>
2025-11-03 23:16:41 +0100

    little fix

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask, render_template, request, jsonify
 from PIL import Image
 import imagehash
@@ -125,5 +124,4 @@ def compare():
 
 if __name__ == '__main__':
     init_db()  # Initialize database on startup
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True,host='0.0.0.0',port=5000)
